b4f947693a17040a04c6da2886e5fa9924fe8e72
PANKAJ SHARMA <147194794+Pankaj-Sharmaa@users.noreply.github.com>
2024-01-10 20:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+<script>
+    function submitForm() {
+      var firstName = document.getElementById("firstName").value;
+      var lastName = document.getElementById("lastName").value;
+      var phoneNumber = document.getElementById("phoneNumber").value;
+      var email = document.getElementById("email").value;
+
+      var result = "First Name: " + firstName + "\nLast Name: " + lastName + "\nPhone Number: " + phoneNumber + "\nEmail ID: " + email;
+
+      alert(result);
+    }
+  </script>
